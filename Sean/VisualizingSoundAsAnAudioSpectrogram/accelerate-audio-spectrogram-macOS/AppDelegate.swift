f91d4a0182bc1374bab71d06bd6520a229d75630
Sean <51236302+Seanmlevine@users.noreply.github.com>
2021-10-01 10:27:41 -0400

    Added Visualization Example via Upload

---
+++ Sean/VisualizingSoundAsAnAudioSpectrogram/accelerate-audio-spectrogram-macOS/AppDelegate.swift
@@ -0,0 +1,14 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The app delegate.
+*/
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet var window: NSWindow!
+}
+
