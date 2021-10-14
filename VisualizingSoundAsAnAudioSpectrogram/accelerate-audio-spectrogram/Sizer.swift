5cc7265983b6dff1262e199491d06da4f53bf1c4
taylorj0772 <90894944+taylorj0772@users.noreply.github.com>
2021-10-14 14:44:50 -0400

    Add files via upload

---
+++ VisualizingSoundAsAnAudioSpectrogram/accelerate-audio-spectrogram/Sizer.swift
@@ -0,0 +1,40 @@
+//
+//  Sizer.swift
+//  accelerate-audio-spectrogram
+//
+//  Created by Joseph Taylor on 10/13/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+/*
+import Foundation
+
+class Sizer {
+    var currHeight: Double
+    var currWidth: Double
+    
+    var fullHeight: Double
+    var fullWidth: Double
+    
+    
+    let screenRect = UIScreen.main.bounds
+    
+    
+    
+    init(currHeight: Double, currWidth: Double) {
+    self.currHeight = currHeight
+    self.currWidth = currWidth
+    }
+    
+    func resize(newHeight: Double, newWidth: Double) {
+        
+    }
+}
+
+
+
+audioSpectrogram.frame.origin.x = (screenRect.size.width * 0.1)
+audioSpectrogram.frame.origin.y = (screenRect.size.height * (1/22))
+audioSpectrogram.frame.size.width = (screenRect.size.width * 0.8)
+audioSpectrogram.frame.size.height = (screenRect.size.height * (7/22))
+
+*/
